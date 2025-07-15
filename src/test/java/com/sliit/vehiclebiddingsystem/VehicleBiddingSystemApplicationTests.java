e329e77d2325d4731c090f933398aa25f1aa8837
Sandaruwan Shanaka <perolopessthesoosa@gmail.com>
2025-07-15 18:23:48 +0530

    feat(tests): add initial application context test

---
+++ src/test/java/com/sliit/vehiclebiddingsystem/VehicleBiddingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.vehiclebiddingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleBiddingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
